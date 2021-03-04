3b10f0c6e663c61091d3bff888e385ac09ef2965
Zhou-ly <1959649788@qq.com>
2021-03-04 23:18:42 +0800

    学习Go语言第五天，浮点型，复数，布尔型

---
+++ Zhou-ly/day05/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"	
+	"math"
+)
+
+
+func main() {
+	fmt.Printf("%f\n", math.Pi)
+	fmt.Printf("%.2f\n", math.Pi)
+
+	// math.MaxFloat32  float32最大值
+	f1 := 1.23456
+	fmt.Printf("%T\n", f1)	// 默认Go语言中的小数都是float64类型
+
+	f2 := float32(1.234)
+	fmt.Printf("%T\n", f2)	// 		显示声明float32类型
+
+	// 注意：float32类型的值不能直接赋值给float64类型的值
+
+}
